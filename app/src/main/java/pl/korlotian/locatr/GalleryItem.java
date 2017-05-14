9095357d938791ad1b818ce3e1ad5f8d6765d97e
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-05-14 19:19:54 +0200

    Add gallery item and flickrfetchr

---
+++ app/src/main/java/pl/korlotian/locatr/GalleryItem.java
@@ -0,0 +1,55 @@
+package pl.korlotian.locatr;
+
+import android.net.Uri;
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+}
