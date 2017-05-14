9ba155e04a43dab7b7ae354ec7356f598bdd7025
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-05-14 19:57:59 +0200

    Add google api client

--- app/src/main/java/pl/korlotian/locatr/LocatrFragment.java
+++ app/src/main/java/pl/korlotian/locatr/LocatrFragment.java
@@ -9,9 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 
 public class LocatrFragment extends Fragment {
     private ImageView mImageView;
+    private GoogleApiClient mClient;
 
     public static LocatrFragment newInstance() {
         return new LocatrFragment();
@@ -21,6 +25,10 @@ public class LocatrFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        mClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
